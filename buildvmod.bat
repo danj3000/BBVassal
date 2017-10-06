3a9b265a638d002d80cc9351022f362e81ce4cda
Dan Johnson <dan@netfield.co.uk>
2017-10-06 22:52:36 +0100

    rollover button first experiments

--- buildvmod.bat
+++ buildvmod.bat
@@ -1,2 +1,2 @@
 del Blood_Bowl_Vassal_1.7.vmod
-7z.exe a -tzip Blood_Bowl_Vassal_1.7.vmod images\*.* sounds\*.* buildfile moduledata Kickoff_Table.html
+7z.exe a -tzip Blood_Bowl_Vassal_1.7.vmod images\*.* sounds\*.* buildfile moduledata Kickoff_Table.html .\out\*.class
