c2bf44cbaaa3f08317a617519aa3644e0ba06ac9
danj3000 <danj3000@outlook.com>
2017-11-03 06:11:40 +0000

    move to ant build scripts and build output directory

--- buildvmod.bat
+++
@@ -1,2 +0,0 @@
-del Blood_Bowl_Vassal_BETA.vmod
-7z.exe a -tzip Blood_Bowl_Vassal_BETA.vmod images\*.* sounds\*.* buildfile moduledata Kickoff_Table.html .\out\*.class
--- extractbuildFile.bat
+++
@@ -1,2 +0,0 @@
-del buildFile
-7z.exe e Blood_Bowl_Vassal_BETA.vmod buildfile
